bb9bea8945b7b2d8fb77cf3d8cfe2cbf7a286820
abhilash-kuvo <abhilash@kuvo.ai>
2025-07-11 12:19:23 +0530

    Updated BuildPage.tsx

--- src/pages/BuildPage.tsx
+++ src/pages/BuildPage.tsx
@@ -1,17 +1,14 @@
-@@ .. @@
--interface AgentRunPageProps {
-+interface BuildPageProps {
+interface BuildPageProps {
    onSubmit: (query: string) => void;
    onNewPlaybook: () => void;
    onMyPlaybooks: () => void;
-@@ .. @@
- }
- 
--const AgentRunPage: React.FC<AgentRunPageProps> = ({ 
-+const BuildPage: React.FC<BuildPageProps> = ({ 
-   onSubmit, 
-   onNewPlaybook, 
-@@ .. @@
- };
- 
--export default AgentRunPage;
+}
+
+const BuildPage: React.FC<BuildPageProps> = ({ 
+  onSubmit, 
+  onNewPlaybook,
+  onMyPlaybooks
+}) => {
+};
+
+export default BuildPage;
